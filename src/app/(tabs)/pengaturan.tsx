28f6331840edc9831183248a3033d0b171a46b37
Isnaeni Kholifatun <isnaenikholifatun26@gmail.com>
2026-09-22 15:09:20 +0200

    perbaikan pengaturan

--- src/app/(tabs)/pengaturan.tsx
+++ src/app/(tabs)/pengaturan.tsx
@@ -1,10 +1,13 @@
 // app/(tabs)/pengaturan.tsx 
+import { SafeAreaView } from "react-native-safe-area-context";
 import { View, Text } from "react-native"; 
 export default function TabPengaturan() { 
-return ( 
-<View style={{ padding: 16 }}> 
-<Text style={{ fontSize: 18, fontWeight: "bold" }}>Jelajah Aman</Text> 
-<Text>Versi 1.0.0</Text> 
-</View> 
-); 
+    return ( 
+        <SafeAreaView> 
+            <View style={{ padding: 16 }}> 
+            <Text style={{ fontSize: 18, fontWeight: "bold" }}>Jelajah Aman</Text> 
+            <Text>Versi 1.0.0</Text> 
+            </View> 
+        </SafeAreaView> 
+    ); 
 } 
