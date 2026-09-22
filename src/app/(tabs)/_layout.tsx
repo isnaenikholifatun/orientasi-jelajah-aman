31453d972c6b77b35aaea5f55b628ea9f33dd25d
Isnaeni Kholifatun <isnaenikholifatun26@gmail.com>
2026-09-22 12:52:55 +0200

    feat: update tab navigation dan pengaturan

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,20 @@
-// app/(tabs)/_layout.tsx 
-import { Tabs } from "expo-router"; 
-  
-export default function TabLayout() { 
-  return ( 
-    <Tabs screenOptions={{ headerShown: true }}> 
-      <Tabs.Screen name="index" options={{ title: "Beranda" }} /> 
-      <Tabs.Screen name="riwayat" options={{ title: "Riwayat" }} /> 
-      <Tabs.Screen name="pengaturan" options={{ title: "Pengaturan" }} /> 
-    </Tabs> 
-  ); 
-} 
+import { Tabs } from "expo-router";
+
+export default function TabLayout() {
+  return (
+    <Tabs
+      screenOptions={{
+        headerShown: true,
+        tabBarStyle: {
+          height: 70,
+          paddingBottom: 10,
+          paddingTop: 5,
+        },
+      }}
+    >
+      <Tabs.Screen name="index" options={{ title: "Beranda" }} />
+      <Tabs.Screen name="riwayat" options={{ title: "Riwayat" }} />
+      <Tabs.Screen name="pengaturan" options={{ title: "Pengaturan" }} />
+    </Tabs>
+  );
+}
--- src/app/(tabs)/pengaturan.tsx
+++ src/app/(tabs)/pengaturan.tsx
@@ -1,12 +1,10 @@
-import { SafeAreaView } from "react-native-safe-area-context";
-import RiwayatList from "../../components/RiwayatList";
-
-export default function TabRiwayat() {
-  const daftarKota = ["Pekalongan", "Jakarta", "Semarang"];
-
-  return (
-    <SafeAreaView style={{ flex: 1, padding: 16 }}>
-      <RiwayatList daftarKota={daftarKota} />
-    </SafeAreaView>
-  );
-}
+// app/(tabs)/pengaturan.tsx 
+import { View, Text } from "react-native"; 
+export default function TabPengaturan() { 
+return ( 
+<View style={{ padding: 16 }}> 
+<Text style={{ fontSize: 18, fontWeight: "bold" }}>Jelajah Aman</Text> 
+<Text>Versi 1.0.0</Text> 
+</View> 
+); 
+} 
